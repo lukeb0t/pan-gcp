6dca19cf3bbd8a6b7f3d52cdcb8941fd7da85396
Luke McCleary <luke.mccleary@gmail.com>
2021-05-11 16:14:43 -0400

    versions

--- versions.tf
+++ versions.tf
@@ -1,5 +1,15 @@
 terraform {
-  required_version = ">= 0.12"
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.67.0"
+    }
+    google-beta = {
+      source = "hashicorp/google-beta"
+        version     = "> 3.0.0"
+    }
+  }
+  required_version = "~> 0.14"
 }
 
 provider "google" {
