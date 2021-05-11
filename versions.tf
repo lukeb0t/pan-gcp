14751a87491de9e44e27f02ff6fbca9ec9757fd5
Luke McCleary <luke.mccleary@gmail.com>
2021-05-11 16:27:42 -0400

    try4

--- versions.tf
+++ versions.tf
@@ -11,17 +11,3 @@ terraform {
   }
   required_version = "~> 0.14"
 }
-
-provider "google" {
-  credentials = var.auth_file
-  project     = var.project_id
-  region      = var.regions[0]
-}
-
-provider "google-beta" {
-  credentials = var.auth_file
-  project     = var.project_id
-  region      = var.regions[0]
-}
-
-data "google_compute_zones" "available" {}
