18911de2bc6e6d87c54f8b3c0d4ecb4ac9f3e768
Luke McCleary <luke.mccleary@gmail.com>
2021-05-11 16:15:30 -0400

    deleted versions

--- versions.tf
+++ versions.tf
@@ -2,11 +2,9 @@ terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = "3.67.0"
     }
     google-beta = {
       source = "hashicorp/google-beta"
-        version     = "> 3.0.0"
     }
   }
   required_version = "~> 0.14"
