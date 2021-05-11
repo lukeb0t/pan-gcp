aa5b3544cc8bde157107bc1be73145ec1944da38
Luke McCleary <luke.mccleary@gmail.com>
2021-05-11 16:23:25 -0400

    provider

--- fw_common.tf
+++ fw_common.tf
@@ -1,3 +1,16 @@
+provider "google" {
+  credentials = var.auth_file
+  project     = var.project_id
+  region      = var.regions[0]
+}
+
+provider "google-beta" {
+  credentials = var.auth_file
+  project     = var.project_id
+  region      = var.regions[0]
+}
+
+
 #-----------------------------------------------------------------------------------------------
 # Create bootstrap bucket for firewalls
 module "bootstrap_common" {
